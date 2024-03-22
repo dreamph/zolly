6a2f88cde412338eedc961f228ade71e4e34ded1
dreamph <dream_ph@hotmail.com>
2024-03-22 23:55:49 +0700

    update logo

--- zolly.go
+++ zolly.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"crypto/tls"
+	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/mattn/go-colorable"
 	"time"
 )
 
 func Start(config *GatewayConfig) error {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		DisableStartupMessage: true,
+	})
 	for _, s := range config.Services {
 		balancerHandler, err := NewBalancer(Config{
 			Path:     s.Path,
@@ -24,6 +28,11 @@ func Start(config *GatewayConfig) error {
 
 		app.Group(s.Path, balancerHandler)
 	}
+
+	out := colorable.NewColorableStdout()
+	mainLogo := logo
+	mainLogo += fmt.Sprintf("Server : http://127.0.0.1:%s", config.Server.Port)
+	_, _ = fmt.Fprintln(out, mainLogo)
 
 	err := app.Listen(":" + config.Server.Port)
 	if err != nil {
